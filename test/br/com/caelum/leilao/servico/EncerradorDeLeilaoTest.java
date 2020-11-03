a2bc2488893081cb9208d85929ce9fd999762e4c
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-11-02 23:28:48 -0300

    Muitas Exceções no Mock

--- test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
+++ test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
@@ -152,4 +152,27 @@ public class EncerradorDeLeilaoTest {
 		inOrder.verify(carteiroFalso, times(1)).envia(leilao1);
 	}
 
+	@Test
+	public void deveDesistirSeDaoFalhaPraSempre() {
+		Calendar antiga = Calendar.getInstance();
+		antiga.set(1999, 1, 20);
+
+		Leilao leilao1 = new CriadorDeLeilao().para("TV de plasma").naData(antiga).constroi();
+		Leilao leilao2 = new CriadorDeLeilao().para("Geladeira").naData(antiga).constroi();
+
+		RepositorioDeLeiloes daoFalso = mock(RepositorioDeLeiloes.class);
+		when(daoFalso.correntes()).thenReturn(Arrays.asList(leilao1, leilao2));
+
+		Carteiro carteiroFalso = mock(Carteiro.class);
+		doThrow(new RuntimeException()).when(daoFalso).atualiza(leilao1);
+		doThrow(new RuntimeException()).when(daoFalso).atualiza(leilao2);
+
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(daoFalso, carteiroFalso);
+
+		encerrador.encerra();
+
+		verify(carteiroFalso, never()).envia(leilao1);
+		verify(carteiroFalso, never()).envia(leilao2);
+	}
+
 }
